48368ea196577f8cb3900a79387f6c69966c4ad1
yoshdia.ibuki <yoshida420ibuki@gmail.com>
2020-06-07 05:20:01 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,8 @@ class ApplicationController < ActionController::Base
     end
     def counts(user)
         @count_microposts = user.microposts.count
+        @count_followings = user.followings.count
+        @count_followers = user.followers.count
     end
     
 end
