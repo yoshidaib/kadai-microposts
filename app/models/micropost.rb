fc3983091daa36274f22788ab382156f68bb8aaa
yoshdia.ibuki <yoshida420ibuki@gmail.com>
2020-06-14 10:58:04 +0000

    deploy micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,6 @@ class Micropost < ApplicationRecord
   belongs_to :user
   validates :content, presence: true, length: {maximum: 255}
   
-  has_many :reverses_of_favorites, class_name: 'Favorites', foreign_key: 'micropost_id', dependent: :destroy
+  has_many :reverses_of_favorites, class_name: 'Favorite', foreign_key: 'micropost_id', dependent: :destroy
   
 end
